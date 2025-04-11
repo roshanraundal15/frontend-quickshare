73ee5dc5e881cb192b9cd61fc6da173d6f9c24fe
Roshan Raundal <131371354+roshanraundal15@users.noreply.github.com>
2025-04-12 03:46:13 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import { ShieldCheck } from 'lucide-react';
 
 const HeroSection = () => {
   const handlePredictNow = () => {
-    window.location.href = 'http://localhost:8501';
+    window.location.href = 'https://agri-predictor.streamlit.app/';
   };
 
   const handleQualityCheck = () => {
